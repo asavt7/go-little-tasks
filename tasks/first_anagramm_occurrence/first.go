0d46a1b83f07af6245d06c131c4c9b7960ff31f7
asavt7 <asavt93@gmail.com>
2021-12-25 00:30:51 +0400

    хеш для отптимизхации

--- tasks/first_anagramm_occurrence/first.go
+++ tasks/first_anagramm_occurrence/first.go
@@ -55,3 +55,76 @@ func FirstAnagramOccurrence(instr string, substr string) int {
 
 	return -1
 }
+
+func FirstAnagramOccurrence_OptHash(instr string, substr string) int {
+	if len(instr) == 0 {
+		if len(substr) > 0 {
+			return -1
+		}
+		return 0
+	}
+
+	in := []rune(instr)
+	searchingHash := hash([]rune(substr))
+
+	searching := make(map[rune]int)
+	for _, i2 := range substr {
+		searching[i2]++
+	}
+	l, r := 0, len(substr)-1
+
+	m := make(map[rune]int)
+	for _, i := range in[l : r+1] {
+		m[i]++
+	}
+
+	substrHash := hash(in[l : r+1])
+
+	found := true
+	if substrHash == searchingHash {
+		for k, _ := range searching {
+			if m[k] != searching[k] {
+				found = false
+				break
+			}
+		}
+		if found {
+			return l
+		}
+	}
+
+	for r < len(in) {
+		if substrHash == searchingHash {
+			found = true
+			for k, _ := range searching {
+				if m[k] != searching[k] {
+					found = false
+					break
+				}
+			}
+			if found {
+				return l
+			}
+		}
+
+		substrHash -= in[l]
+		m[in[l]]--
+		l++
+		r++
+		if r == len(in) {
+			return -1
+		}
+		m[in[r]]++
+		substrHash += in[r]
+	}
+
+	return -1
+}
+
+func hash(s []rune) int32 {
+	var r int32
+	for _, i2 := range s {
+		r += i2
+	}
+	return r
+}
--- tasks/first_anagramm_occurrence/first_test.go
+++ tasks/first_anagramm_occurrence/first_test.go
@@ -42,4 +42,46 @@ func TestFirstAnagramOccurrence(t *testing.T) {
 		})
 	}
 
+	for _, s := range tt {
+		t.Run(s.in, func(t *testing.T) {
+			if got := first.FirstAnagramOccurrence_OptHash(s.in, s.substr); got != s.exp {
+				t.Errorf("%+v substr: %d != got: %d", s, s.exp, got)
+			}
+		})
+	}
+
+}
+
+func BenchmarkFirstAnagramOccurrence_little(b *testing.B) {
+	in := "abcdeqwertyuiabcd"
+	substr := "bcd"
+
+	for i := 0; i < b.N; i++ {
+		_ = first.FirstAnagramOccurrence(in, substr)
+	}
+}
+func BenchmarkFirstAnagramOccurrence_OptHash_little(b *testing.B) {
+	in := "abcdeqwertyuiabcd"
+	substr := "bcd"
+
+	for i := 0; i < b.N; i++ {
+		_ = first.FirstAnagramOccurrence_OptHash(in, substr)
+	}
+}
+
+func BenchmarkFirstAnagramOccurrence_middle(b *testing.B) {
+	in := "abcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabSUCKcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcd"
+	substr := "CKUS"
+
+	for i := 0; i < b.N; i++ {
+		_ = first.FirstAnagramOccurrence(in, substr)
+	}
+}
+func BenchmarkFirstAnagramOccurrence_OptHash_middle(b *testing.B) {
+	in := "abcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabSUCKcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcdabcdeqwertyuiabcd"
+	substr := "CKUS"
+
+	for i := 0; i < b.N; i++ {
+		_ = first.FirstAnagramOccurrence_OptHash(in, substr)
+	}
 }
