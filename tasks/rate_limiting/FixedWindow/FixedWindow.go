603368be74261bbfdc9a0cfcb350a20eed0fc7ce
asavt7 <asavt93@gmail.com>
2022-01-01 23:05:20 +0400

    simple fixed window ratelimiter

---
+++ tasks/rate_limiting/FixedWindow/FixedWindow.go
@@ -0,0 +1,40 @@
+package FixedWindow
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type FixedWindowLimiter struct {
+	mu sync.Mutex
+
+	currentRqCount, maxRq int64
+	windowSize            time.Duration
+}
+
+func NewFixedWindowLimiter(maxRq int, windowSize time.Duration) *FixedWindowLimiter {
+	r := &FixedWindowLimiter{maxRq: int64(maxRq), windowSize: windowSize}
+
+	return r
+}
+
+func (r *FixedWindowLimiter) startUpdateWindowWorker() {
+	go func() {
+		for {
+			time.Sleep(r.windowSize)
+			atomic.SwapInt64(&(r.currentRqCount), 0)
+		}
+	}()
+}
+
+func (r *FixedWindowLimiter) AllowRequest() bool {
+	if r.currentRqCount >= r.maxRq {
+		fmt.Println("blocked")
+		return false
+	}
+	atomic.AddInt64(&(r.currentRqCount), 1)
+	fmt.Printf("allowd rq_count %d\n", r.currentRqCount)
+	return true
+}
---
+++ tasks/rate_limiting/FixedWindow/FixedWindow_test.go
@@ -0,0 +1,43 @@
+package FixedWindow_test
+
+import (
+	"github.com/asavt7/go-little-tasks/tasks/rate_limiting/FixedWindow"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestFixedWindowLimiter_AllowRequest(t *testing.T) {
+	r := FixedWindow.NewFixedWindowLimiter(10, time.Second)
+
+	for i := 0; i < 10; i++ {
+		if !r.AllowRequest() {
+			t.Error("should allow rq")
+		}
+	}
+
+	if r.AllowRequest() {
+		t.Error("should not allow rq")
+	}
+
+}
+
+func TestFixedWindowLimiter_AllowRequest_concurrent(t *testing.T) {
+	r := FixedWindow.NewFixedWindowLimiter(10, time.Second)
+
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			if !r.AllowRequest() {
+				t.Error("should allow rq")
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	if r.AllowRequest() {
+		t.Error("should not allow rq")
+	}
+
+}
